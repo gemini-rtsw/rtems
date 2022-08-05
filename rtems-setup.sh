42cca3f855d243d5b68fadcff9c14c005bbad665
fkraemer <fkraemer@gemini.edu>
2022-08-04 23:19:09 -1000

    added rtems-source-builder and rtems as submodules to be able to update to newer versions more easily and to link vesions (i.e. git hashes) to script and patch sets

--- rtems-setup.sh
+++ rtems-setup.sh
@@ -8,18 +8,19 @@ export RTEMS_BSPS="mvme2307 beatnik mvme3100"
 #production 
 #export RTEMS_BASE=/gem_base/targetOS/RTEMS/
 #testing
-export RTEMS_BASE=/gem_base/test/targetOS/RTEMS
+export RTEMS_BASE=/gem_base/test/fkraemer/targetOS/RTEMS
 export RTEMS_INSTALL_DIR=rtems
 export RTEMS_ROOT=${RTEMS_BASE}/${RTEMS_INSTALL_DIR}/${RTEMS_VERSION}
 export PATH=${RTEMS_ROOT}/bin:${PATH}
 export RTEMS_MAKEFILE_PATH=${RTEMS_ROOT}/${RTEMS_ARCH}/${RTEMS_BSP}
 export RTEMS_SHARE_PATH=${RTEMS_ROOT}/share/rtems${RTEMS_VERSION}
 
-echo "installing bison, flex, texinfo, python2-devel, spax"
-echo "sudo permissions needed..."
-sudo dnf install -y bison flex texinfo python2-devel spax
-
-sudo alternatives --set python /usr/bin/python3
+## Uncomment to install dependencies
+#echo "installing bison, flex, texinfo, python2-devel, spax"
+#echo "sudo permissions needed..."
+#sudo dnf install -y bison flex texinfo python2-devel spax
+#
+#sudo alternatives --set python /usr/bin/python3
 
 #Need install location
 if [ ! -d ${RTEMS_BASE}/${RTEMS_INSTALL_DIR} ]
@@ -29,29 +30,14 @@ else
     echo "Install path exists and Ready!" 
 fi
 
-cd ${RTEMS_BASE}
-
-if [ ! -d "rsb" ] 
-then
-    git clone https://github.com/RTEMS/rtems-source-builder.git rsb
-else
-    echo "RSB path exists and Ready!" 
-fi
-cd rsb
-git pull --ff-only --all
-git checkout 5
+pushd vendor/rtems-source-builder
 
 cd rtems
 ../source-builder/sb-set-builder --prefix=${RTEMS_ROOT} 5/rtems-powerpc
-cd ../../${RTEMS_INSTALL_DIR}
 
 # building kernel
-mkdir kernel
-cd kernel
-git clone git://git.rtems.org/rtems.git rtems
-cd rtems
-git pull --all
-git checkout 5
+popd
+pushd vendor/rtems
 
 # build and install bsp
 ./bootstrap -c && ./rtems-bootstrap
@@ -62,7 +48,6 @@ for bsp in $RTEMS_BSPS; do
     cd ..
     mkdir ${bsp}
     cd ${bsp}
-    #../rtems/configure --prefix=${RTEMS_ROOT} --target=powerpc-rtems5 --enable-rtemsbsp=${bsp} --enable-posix --enable-c++ --enable-networking --enable-tests
     ../rtems/configure --prefix=${RTEMS_ROOT} --target=powerpc-rtems5 --enable-rtemsbsp=${bsp}  --enable-posix --enable-c++ --enable-networking --enable-tests
     make -j16 all
     make install
