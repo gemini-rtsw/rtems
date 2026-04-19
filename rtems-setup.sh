05389f182e71eb81d0807c7f377c2f166dd3ea66
Matt Rippa <matt.rippa@noirlab.edu>
2026-04-18 20:24:27 -1000

    RTEMS-858: Build for Deployment 6.2 on EL9
    * Update rtems setup for RTEMS 6.2
    
    * Track gem-ci testing/2026q2 for RTEMS EL9

--- rtems-setup.sh
+++ rtems-setup.sh
@@ -2,33 +2,35 @@
 checkout=$(git log --pretty=format:'%h' -n 1)
 
 set -e
+set -o pipefail
 set -x
 
 ## please edit to your needs
-# RTEMS vesion
+# RTEMS version
 export RTEMS_VERSION=6
 
 ## There can be either a RTEMS_RELEASE or RTEMS_SOURCE_BUILDER_REVISION 
 ## (i.e. specific git commit hash).
-## RTEMS_RELEASE has precedence, meaning if set, this will be build
-export RTEMS_RELEASE=6.1-rc4
+## RTEMS_RELEASE has precedence, meaning if set, this will be built.
+## For a non-rc release, use the release tarballs instead of git hashes.
+export RTEMS_RELEASE=6.2
 
-## RSB commit hashes
+## RSB commit hash. Used only when RTEMS_RELEASE is unset, e.g. via -G.
 ## comment out RTEMS_RELEASE if you want to build from git revision and
 ## specify the revision you want to be checked out
-export RTEMS_SOURCE_BUILDER_REVISION=989ec6342f410f21e6585e4828a4c5f4f8da5e83
+export RTEMS_SOURCE_BUILDER_REVISION=3814cb0e7f86cca2be403eac831f9bf571984659
 
 export RTEMS_RELEASE_URL=https://ftp.rtems.org/pub/rtems/releases
 
-# RTEMS-deploymeny revision (i.e. git hash)
-export RTEMS_DEPLOYMENT_REVISION=76552d56975aceae36ec68e890c440fb7ef74228
+# RTEMS-deployment revision (i.e. git hash). Used only when RTEMS_RELEASE is unset.
+export RTEMS_DEPLOYMENT_REVISION=612fc665fa49c49a37b00dd84664261da74f6fc5
 ## either legacy or libbsd
 export RTEMS_LEGACY_OR_LIBBSD="legacy"
 
 export RTEMS_BASE=/gem_base/targetOS/RTEMS/rtems
 
 # options
-while getopts 'V:R:u:b:' OPTION
+while getopts 'GV:R:u:b:' OPTION
 do
 	case "$OPTION" in
 		V)
@@ -60,10 +62,10 @@ export RTEMS_ROOT=${RTEMS_BASE}/${RTEMS_VERSION}
 rm -rf rtems-source-builder rtems-deployment
 
 if [[ "$RTEMS_RELEASE" == *"rc"* ]]; then
-	curl ${RTEMS_RELEASE_URL}/${RTEMS_VERSION}/rc/${RTEMS_RELEASE}/sources/rtems-source-builder-${RTEMS_RELEASE}.tar.xz | tar xJf -
+	curl -fsSL ${RTEMS_RELEASE_URL}/${RTEMS_VERSION}/rc/${RTEMS_RELEASE}/sources/rtems-source-builder-${RTEMS_RELEASE}.tar.xz | tar xJf -
 	mv rtems-source-builder-${RTEMS_RELEASE} rtems-source-builder
 elif [ "$RTEMS_RELEASE" != "" ]; then
-	curl ${RTEMS_RELEASE_URL}/${RTEMS_VERSION}/${RTEMS_RELEASE}/sources/rtems-source-builder-${RTEMS_RELEASE}.tar.xz | tar xJf -
+	curl -fsSL ${RTEMS_RELEASE_URL}/${RTEMS_VERSION}/${RTEMS_RELEASE}/sources/rtems-source-builder-${RTEMS_RELEASE}.tar.xz | tar xJf -
 	mv rtems-source-builder-${RTEMS_RELEASE} rtems-source-builder
 else
 	git clone https://gitlab.rtems.org/rtems/tools/rtems-source-builder.git
@@ -77,16 +79,27 @@ else
 	cd ../
 fi
 
-git clone https://gitlab.rtems.org/rtems/tools/rtems-deployment.git
+if [[ "$RTEMS_RELEASE" == *"rc"* ]]; then
+	curl -fsSL ${RTEMS_RELEASE_URL}/${RTEMS_VERSION}/rc/${RTEMS_RELEASE}/sources/rtems-deployment-${RTEMS_RELEASE}.tar.xz | tar xJf -
+	mv rtems-deployment-${RTEMS_RELEASE} rtems-deployment
+elif [ "$RTEMS_RELEASE" != "" ]; then
+	curl -fsSL ${RTEMS_RELEASE_URL}/${RTEMS_VERSION}/${RTEMS_RELEASE}/sources/rtems-deployment-${RTEMS_RELEASE}.tar.xz | tar xJf -
+	mv rtems-deployment-${RTEMS_RELEASE} rtems-deployment
+else
+	git clone https://gitlab.rtems.org/rtems/tools/rtems-deployment.git
+	cd rtems-deployment
+	git checkout ${RTEMS_DEPLOYMENT_REVISION}
+	cd ../
+fi
+
 cd rtems-deployment
-git checkout ${RTEMS_DEPLOYMENT_REVISION}
 #sed -i -e "s#^%define\ name\ .*#%define name rtems#" \
 #       -e "s#^Release:\ .*#Release: ${checkout}.%{rsb_revision}%{?dist}#" pkg/rpm.spec.in
 mkdir -p out/buildroot/BUILD
 mkdir -p out/buildroot/RPMS/x86_64
 
 ## comment out for build for EPICS core devs
-../rtems-source-builder/source-builder/sb-set-builder --url=https://ftp.rtems.org/pub/rtems/cache/rsb/main 6/rtems-powerpc 
+../rtems-source-builder/source-builder/sb-set-builder --url=https://ftp.rtems.org/pub/rtems/cache/rsb/main ${RTEMS_VERSION}/rtems-powerpc
 ./waf configure --prefix=${RTEMS_ROOT} --rsb=../rtems-source-builder --rpm-config=../gemini-config.ini --rpm-config-value=gemini_version=${checkout}
 
 ## .. and use this one for EPICS core devs
